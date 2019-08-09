f54f86e344d5d7bd4d9be2dd1d9e04c3b4fd406a
Zachary Brandon Berman <zbberman272@gmail.com>
2019-08-09 13:48:22 -0400

    started implementing rules

---
+++ src/ruleCheckers.js
@@ -0,0 +1,52 @@
+
+//------------------
+//  hitting rules
+//------------------
+
+//if the dealers face-up card is greater than 6 you should continue
+//hitting until your hand has a value of at least 17
+
+//if the dealers face-up card is less than seven but more than 3,
+//you should hit until the value of your hand hits a minimum of 12
+
+//if the dealers face-up card is a two or three, you should hit until
+//your hand is worth 13 or more
+
+//if you have a soft hand such as an ace and a five, you should
+//continue hitting until the value of your hand is 18 or more
+
+//always hit A7 (soft 18) when the dealers upcard is 9,10,A
+//goal is to get soft 19-21 or hard 17-21
+
+//always stand with a pair of 9's when the dealers upcard is a 7
+
+//stand on hard 16 against a dealers 10 upcard if your 16 is multi-card 
+
+
+//------------------
+//  double rules
+//------------------
+
+//you should double down if your initial cards have a combined value
+//of 11
+
+//if your cards total 10, double down if the dealers upcard is 9 or less
+
+//if your initial two cards have a total value of nine, you should
+//only double down if the dealer is showing a two through six
+
+//always double down A2 - A7 when the dealers upcard ia a 5 or 6
+
+//------------------
+//  split rules
+//------------------
+
+//if you have a pair of aces or eights, you should split your hand
+
+//if you have a pair of 2's, 3's, or 7's you should split your hand
+//unless the dealer is showing an eight, nine, ten, face card, or ace
+
+//you should only split sixes if the dealer's face-up card is a
+//two, three, four, five, or six
+
+//do not split your hand when you have a pair of 4's, 5's, or 10's
